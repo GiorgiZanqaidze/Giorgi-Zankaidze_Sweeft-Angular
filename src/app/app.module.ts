779ba3e9316b96dc555df53ec3d88fb5e2411509
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-09-26 08:03:55 +0400

    doc: add folders for users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserItemComponent } from './users-list/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
